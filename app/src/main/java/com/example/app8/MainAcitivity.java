ca8bb972b658ea294d8930ced241dc63ec8bdb3f
duluptt <101175501+michiiin@users.noreply.github.com>
2023-10-23 04:25:52 +0900

    all files

--- app/src/main/java/com/example/app8/MainAcitivity.java
+++ app/src/main/java/com/example/app8/MainAcitivity.java
@@ -17,23 +17,8 @@ public class MainAcitivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        Button button_logout = findViewById(R.id.button_logout);
-        button_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //로그아웃 하기
-                mFirebaseAuth.signOut();
-                Intent intent = new Intent(MainAcitivity.this,loginActivity.class );
-                startActivity(intent);
-
 
             }
-        });
+        }
 
-
-    }
-
-
-}
+        
---
+++ app/src/main/java/com/example/app8/OcrActivity.java
@@ -0,0 +1,14 @@
+package com.example.app8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OcrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr);
+    }
+}
